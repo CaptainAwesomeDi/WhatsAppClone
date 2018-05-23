b5bb815336200410bfafe8de81bf7f51e5de63a0
Di Wu <wd1991610@gmail.com>
2018-05-22 21:36:51 -0700

    finish seeding the database added resolvers and connectors

--- server/data/connectors.js
+++ server/data/connectors.js
@@ -1,10 +1,12 @@
 import Sequelize from 'sequelize';
+import { _ } from 'lodash';
+import faker from 'faker';
 
 // intitialize database
 const db = new Sequelize('chatty', null, null, {
 	dialect: 'sqlite',
 	storage: './chatty.sqlite',
-	logging: true,
+	logging: false,
 });
 
 // define groups
@@ -39,8 +41,48 @@ MessageModel.belongsTo(GroupModel);
 // groups have multiple users
 GroupModel.belongsToMany(UserModel,{ through: 'GroupUser'});
 
+//create fake starter data
+const GROUPS = 4;
+const USERS_PER_GROUP = 5;
+const MESSAGES_PER_USER = 5;
+faker.seed(123);
+
+//faker creates fake data
+db.sync({force: true}).then(() => _.times(GROUPS, () => GroupModel.create({
+	name: faker.lorem.words(3),
+}).then(group => _.times(USERS_PER_GROUP, () => {
+	const password = faker.internet.password();
+	return group.createUser({
+		email: faker.internet.email(),
+		username: faker.internet.userName(),
+		password,
+	}).then((user) => {
+		console.log(
+			'{email, username, password}',
+			`{${user.email}, ${user.username}, ${password}}`
+		);
+		_.times(MESSAGES_PER_USER, () => MessageModel.create({
+			userId: user.id,
+			groupId: group.id,
+			text: faker.lorem.sentences(3),
+		}));
+		return user;
+	});
+})).then((userPromises) => {
+	// make users friends with all users in the group
+	Promise.all(userPromises).then((users) => {
+		_.each(users, (current, i) => {
+			_.each(users, (user, j) => {
+				if (i !== j) {
+					current.addFriend(user);
+				}
+			});
+		});
+	});
+})));
+
 const Group = db.models.group;
 const Message = db.models.message;
-const User = db.models.message;
+const User = db.models.user;
 
 export { Group, Message, User };
