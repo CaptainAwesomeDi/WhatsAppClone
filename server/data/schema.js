0343cb233adaa571bfce774e4528ef0b4a2afc19
Di Wu <wd1991610@gmail.com>
2018-05-22 20:29:18 -0700

    finished adding model and querying schema

--- server/data/schema.js
+++ server/data/schema.js
@@ -1,10 +1,49 @@
-export const Schema = [
-  `type Query {
-    testString: String
-  }
+export const Schema = [`
+# declare custom scalars
+scalar Date
+
+# a group chat entity
+type Group {
+  id: Int! # unique id for the group
+  name: String # name of the group
+  users: [User]! # users in the group
+  messages: [Message] # messages sent to the group
+}
+
+# a user
+type User {
+ id: Int!
+ email: String!
+ username: String
+ messages: [Message]
+ groups: [Group]
+ friends: [User]
+}
+
+type Message {
+  id: Int!
+  to: Group!
+  from: User!
+  text: String!
+  createdAt: Date!
+}
+
+#query for types
+type Query {
+  # Return a user by their email or id
+  user(email: String, id: Int): User
+
+  # Return messages sent by a user via userId
+  # Return messages sent to a group via groupId
+  messages(groupId: Int, userId: Int): [Message]
+
+  # Return a group by its id
+  group(id: Int!): Group
+}
+
   schema {
     query: Query
-  }`,
-];
+  }
+`];
 
 export default Schema;
